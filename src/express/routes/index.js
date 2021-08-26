14071b9b946cedd306bd3a6eec21a25856b58914
Alexander <knighter@rambler.ru>
2021-08-26 11:29:25 +0900

    Pub templates

--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const { Router } = require(`express`);
 const indexRouter = new Router();
 
@@ -20,5 +22,9 @@ indexRouter.get(`/admin-comments`, (req, res) => res.render(`comments`));
 indexRouter.get(`/admin-publications`, (req, res) => res.render(`my`));
 
 indexRouter.get(`/admin-add-new-post-empty`, (req, res) => res.render(`post`));
+
+indexRouter.get(`/admin-categories`, (req, res) => res.render(`all-categories`));
+
+indexRouter.get(`/register`, (req, res) => res.render(`registration`));
 
 module.exports = indexRouter;
