94ad89f12b6e37d950abdadd9ef80b6f53f9984f
Alexander <knighter@rambler.ru>
2021-10-01 13:01:46 +0900

    Publication Ids and comments

--- src/constants.js
+++ src/constants.js
@@ -4,9 +4,12 @@ const DEFAULT_COMMAND = `--help`;
 const USER_ARGV_INDEX = 2;
 const ExitCode = 0;
 
+const MAX_ID_LENGTH = 6;
+
 module.exports =
 {
     DEFAULT_COMMAND,
     USER_ARGV_INDEX,
+    MAX_ID_LENGTH,
     ExitCode
 };
